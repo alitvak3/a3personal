0ddbdca2c2ff8cd9df08c47be58e6323d427b350
alitvak3 <alitvak3@gmail.com>
2021-02-14 23:15:54 -0500

    export other pbetween fucntions

--- R/between.R
+++ R/between.R
@@ -6,17 +6,17 @@
 pbetween.norm <- function(q1, q2, mean = 0, sd = 1){
      pnorm(q2, m, s)-pnorm(q1, m, s)
 }
-
+#' @export
 #' @describeIn pbetween Poisson
 pbetween.pois <- function(q1, q2, l){
      ppois(q2, l)-ppois(q1, l)
 }
-
+#' @export
 #' @describeIn pbetween Student's t
 pbetween.t <- function(q1, q2, df){
      pt(q2, df)-pbinom(q1, df)
 }
-
+#' @export
 #' @describeIn pbetween Binomial
 pbetween.binom <- function(q1, q2, size, prob){
      pbinom(q2, size, p)-pbinom(q1, size, p)
